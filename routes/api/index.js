f914db7474c36eb7b8681002568758142e77df46
KDash201 <KVonMadison@Yahoo.com>
2022-02-23 21:38:12 -0500

    Integrate with the Front End and Refactor

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const commentRoutes = require("./comment-routes");
 const pizzaRoutes = require("./pizza-routes");
+
+//  comments assocaited with specified pizzas
+router.use("/comments", commentRoutes);
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
 router.use("/pizzas", pizzaRoutes);
